d5a149374b1a28242f7b363ff23733fe764eae71
CHC <chc@openspy.net>
2025-04-25 22:07:45 -0400

    seeder fixes

--- sql/import.sh
+++ sql/import.sh
@@ -16,13 +16,13 @@ mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD -h $MYSQL_HOST --database=Peerch
 
 mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD -h $MYSQL_HOST --database=GameTracker < /sql/user_seed.sql
 
+echo 'seed rabbitmq'
+python3 /sql/rabbitmqadmin declare exchange -H $RABBITMQ_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.core type=topic durable=true
+python3 /sql/rabbitmqadmin declare exchange -H $RABBITMQ_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.master type=topic durable=true
+python3 /sql/rabbitmqadmin declare exchange -H $RABBITMQ_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.natneg type=topic durable=true
+python3 /sql/rabbitmqadmin declare exchange -H $RABBITMQ_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.gamestats type=topic durable=true
+python3 /sql/rabbitmqadmin declare exchange -H $RABBITMQ_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=presence.core type=topic durable=true
 
-python3 /sql/rabbitmqadmin declare exchange -H $MYSQL_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.core type=topic durable=true
-python3 /sql/rabbitmqadmin declare exchange -H $MYSQL_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.master type=topic durable=true
-python3 /sql/rabbitmqadmin declare exchange -H $MYSQL_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.natneg type=topic durable=true
-python3 /sql/rabbitmqadmin declare exchange -H $MYSQL_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=openspy.gamestats type=topic durable=true
-python3 /sql/rabbitmqadmin declare exchange -H $MYSQL_HOST --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS --vhost=$RABBITMQ_DEFAULT_VHOST name=presence.core type=topic durable=true
 
-
-curl -X POST "http://$HTTP_API_URL/v1/Game/SyncToRedis" -H  "accept: application/json" -H  "APIKey: $HTTP_API_KEY"
-curl -X POST "http://$HTTP_API_URL/v1/Group/SyncToRedis" -H  "accept: application/json" -H  "APIKey: $HTTP_API_KEY"
+curl -v -X POST "http://$HTTP_API_URL/v1/Game/SyncToRedis" -H  "accept: application/json" -H  "APIKey: $HTTP_API_KEY"
+curl -v -X POST "http://$HTTP_API_URL/v1/Group/SyncToRedis" -H  "accept: application/json" -H  "APIKey: $HTTP_API_KEY"
